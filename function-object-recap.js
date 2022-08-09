cc436abbeb0113dfae8996ff32d5a36fae73d73c
rasel726 <md.rasel2726@gmail.com>
2022-08-09 19:23:16 +0600

    added

---
+++ function-object-recap.js
@@ -0,0 +1,28 @@
+/**
+ * -----------------
+ * SIX CORE CONCEPTS
+ * -----------------
+ * 1. Variable (declare, naming convention, variable types, primitives, math operations)
+ * 2. Conditionals (comparison: >, <, >=, <=, ===, !==, if-else, if-else if-else)
+ * 3. Array (declare, index, get values, set values, indexOf, push, pop)
+ * 4. Loop (for loop, while loop, break, continue)
+ * 5. Function (declare, parameters, return, call, use the returned value form a function)
+ * 6. Object (declare, properties, keys, values, get prop value, set value, loop object properties)
+ */
+
+/**
+ * 1. Google: function declaration VS funchtion expression
+ * 2. Google: arrow function
+ * 3. loop VS function between defference
+ * 4. array VS object between defference
+ * 5. integer VS float between defference
+ * 6. == VS == 
+ * 7. diferencec between: var, let, const
+ */
+
+/**
+ * 1. Celsius ---> Farenheit
+ * 2. Farenheit ---> Celsisu
+ * 3. Simple Interest: principal, interest rate, time ---> interest ammount
+ * 4. Complex Interest: 
+ */
